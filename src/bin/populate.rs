39e0d81d7db8ec01e95c4e5396d3cd984d25f6ce
riemann <aleqvids@gmail.com>
2025-06-12 15:46:38 +0300

    feat: updates

--- src/bin/populate.rs
+++ src/bin/populate.rs
@@ -8,7 +8,7 @@ use serde::Deserialize;
 use std::env;
 use std::fs;
 use std::time::{Duration, Instant};
-use tokio::time::{Duration as TokioDuration, sleep};
+use tokio::time::sleep;
 use tracing::{error, info, warn};
 
 use miden_client::{
